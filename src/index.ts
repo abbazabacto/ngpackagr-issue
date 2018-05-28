8edbcc0a6203aff2e9245776c3bfb97bd0037ad8
abbazabacto <abbazabacto@gmail.com>
2018-05-28 13:35:35 +0200

    separate module from index (re-export)

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,3 @@
-import { NgModule } from '@angular/core';
-import { CoreModule } from '@abbazabacto/ngpackagr-issue/core';
-import { FooModule } from '@abbazabacto/ngpackagr-issue/foo';
-
-@NgModule({
-  imports: [
-    CoreModule,
-    FooModule,
-  ],
-  exports: [
-    CoreModule,
-    FooModule,
-  ]
-})
-export class NgPackagrIssueModule {}
+export * from './module';
+export * from './core';
+export * from './foo';
